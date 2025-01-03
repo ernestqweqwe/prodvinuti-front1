01c71298a4739df34114ada3662fb2be0f8ecabc
Ernestas Yushkiavichus <ernest.bel@mail.ru>
2025-01-03 08:58:17 +0300

    создание файла output с уникальным хешем

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    // entry: './src/index.js' забись равносильна снизу
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, 'build')
+    }
+
+}
