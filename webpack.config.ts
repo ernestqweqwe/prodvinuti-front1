77c37c17d02e6d3b5b39c97fe84322af26e8ab9d
Ernestas Yushkiavichus <ernest.bel@mail.ru>
2025-01-03 16:10:03 +0300

    использование import и конфиг на ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,13 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+import  path from 'path';
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
-module.exports = {
+
+
+const config: webpack.Configuration = {
+  
     // entry: './src/index.js' забись равносильна снизу
-    mode: 'development',
+    mode: 'production',
     entry: path.resolve(__dirname, 'src', 'index.ts'),
     output: {
         filename: "[name].[contenthash].js",
@@ -32,3 +35,5 @@ module.exports = {
       },
 
 }
+
+export default config;
