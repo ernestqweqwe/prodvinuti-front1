1ddf753306f0af19db54435a1468750326e9eb57
Ernestas Yushkiavichus <ernest.bel@mail.ru>
2025-01-04 20:12:47 +0300

    7) темная и светлая тема + переключение между ними

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,24 @@ import './styles/index.scss'
 import { Link, Route, Routes } from 'react-router-dom'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
 import { MainPageAsync } from './pages/MainPage/MainPage.async'
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
+
+export enum Theme {
+  LIGHT = 'light',
+  DARK = 'dark'
+}
 
 const App = () => {
+
+  const [theme, setTheme] = useState<Theme>(Theme.DARK)
+
+  const toggleTheme = () => {
+    setTheme(theme === Theme.DARK?Theme.LIGHT:Theme.DARK)
+  }
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle</button>
       <Link to={'/'}>Главная страница</Link>
       <Link to={'/about'}>О сайте</Link>
        
