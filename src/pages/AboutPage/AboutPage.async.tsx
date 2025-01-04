1b2dbe237f867feff63c7e5c40dae49b2a18a46c
Ernestas Yushkiavichus <ernest.bel@mail.ru>
2025-01-04 16:52:18 +0300

    6) исскуственная задержка для загрузки сьраницы

--- src/pages/AboutPage/AboutPage.async.tsx
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -1,4 +1,7 @@
 import { lazy } from "react";
 
+export const AboutPageAsync = lazy(()=> new Promise(resolve => {
+// @ts-ignore
 
-export const AboutPageAsync = lazy(() => import('./AboutPage'))
+    setTimeout(()=>resolve(import('./AboutPage')),1500)
+}))
--- src/pages/MainPage/MainPage.async.tsx
+++ src/pages/MainPage/MainPage.async.tsx
@@ -1,4 +1,7 @@
 import { lazy } from "react";
 
+export const MainPageAsync = lazy(()=> new Promise(resolve => {
+// @ts-ignore
 
-export const MainPageAsync = lazy(()=> import('./MainPage'))
+    setTimeout(()=>resolve(import('./MainPage')),1500)
+}))
