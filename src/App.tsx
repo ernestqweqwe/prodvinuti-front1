9064407276426c478ab6bfbce01cc63436039595
Ernestas Yushkiavichus <ernest.bel@mail.ru>
2025-01-04 16:32:41 +0300

    6) генерация несколькич файлов js(чанков) вместо одного

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './index.scss'
 import { Link, Route, Routes } from 'react-router-dom'
-import AboutPage from './pages/AboutPage/AboutPage'
-import MainPage from './pages/MainPage/MainPage'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
+import { MainPageAsync } from './pages/MainPage/MainPage.async'
 
 const App = () => {
   return (
@@ -10,8 +10,8 @@ const App = () => {
       <Link to={'/about'}>О сайте</Link>
        
       <Routes>
-        <Route path={'/about'} element={<AboutPage/>}/>
-        <Route path={'/'} element={<MainPage/>}/>
+        <Route path={'/about'} element={<AboutPageAsync/>}/>
+        <Route path={'/'} element={<MainPageAsync/>}/>
       </Routes>
     </div>
   )
